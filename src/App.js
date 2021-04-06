54aa2edaa49dadf36ea4b262049b5f1335c8323e
N M <qytln4yh8g@blackdynamix.com>
2021-04-06 18:03:21 +0430

    add home page and first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import {HomePage} from "./pages/homePage/HomePage";
+
+class App extends Component {
+  render() {
+    return <div>Hello World To<HomePage/></div>;
+  }
+}
+
+export default App;
