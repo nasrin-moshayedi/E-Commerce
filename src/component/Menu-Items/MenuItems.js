641e92c60d766e346f32ce59f70d7d9a04fe22a7
N M <qytln4yh8g@blackdynamix.com>
2021-04-06 18:37:39 +0430

    update home page style

--- src/component/Menu-Items/MenuItems.js
+++ src/component/Menu-Items/MenuItems.js
@@ -3,10 +3,12 @@ import "./menu-item.styles.scss"
 const MenuItem = ({title, bg, size}) => {
     console.log(bg, title);
     return(
-        <div style={{backgroundImage: `url(${bg})`}} className={`${size} menu-item`}>
-            <div className="content">
-                <h1 className="title">{title}</h1>
-                <span className="subtitle">SHOP NOW</span>
+        <div className={`${size} menu-item`}>
+            <div style={{backgroundImage: `url(${bg})`}} className={"background-image"}>
+                <div className="content">
+                    <h1 className="title">{title.toUpperCase()}</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
             </div>
         </div>
     )
