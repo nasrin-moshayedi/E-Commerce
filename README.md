d6ba7e1dbb822b3dacfce773825a88b232b7bdb1
N M <qytln4yh8g@blackdynamix.com>
2021-04-09 17:10:50 +0430

    add header

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # E-Commerce
+
+
+E-commerce web site by react redux 
+
+
+Udemy course for master 
+
+start => yarn start
--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import {HomePage} from "./pages/homePage/HomePage";
 import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/ShopPage/ShopPage";
+import Header from "./component/Header/Header";
 
 class App extends Component {
-  render() {
-      console.log(this.props)
-    return <div>
-        <Switch>
-            <Route exact path={"/"} component={HomePage}/>
-            <Route exact path={`/shop`} component={ShopPage}/>
-        </Switch>
-    </div>;
-  }
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path={"/"} component={HomePage}/>
+                    <Route exact path={`/shop`} component={ShopPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
